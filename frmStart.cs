e7ece28950cd0b92a7f0972da13d2eafaebcc7d0
Falcon303 <50917954+Falcon303@users.noreply.github.com>
2019-09-23 23:00:45 +0700

    change some code in frm Start.cs

--- frmStart.cs
+++ frmStart.cs
@@ -46,49 +46,7 @@ namespace skelot
         {
 
         }
-  private void btnSua_Click_2(object sender, EventArgs e)
-        {
-            int luongnv = 0;
-            try
-            {
-                luongnv = int.Parse(txtLuong.Text);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Kiểu dữ liệu của lương phải là kiểu số !");
-            }
-            if (txtMaNv.Text.Trim() == "")
-                MessageBox.Show("Mã nhân viên không được để trống !");
-            else if (txtTenNv.Text.Trim() == "")
-                MessageBox.Show("Tên nhân viên không được để trống !");
-            else if (luongnv == 0)
-                MessageBox.Show("Sửa thất bại !");
-            else
-                nvb.updateNV(txtMaNv.Text, txtTenNv.Text, dtpNgaysinh.Value.ToString("dd/MM/yyyy"), txtDiachi.Text, luongnv, cbxPhongban.SelectedValue.ToString(), cbxDuan.SelectedValue.ToString());
-            Quanlynhanvien_Load(sender, e);
-        }
-		        private void btnThem_Click(object sender, EventArgs e)
-        {
-            int luongnv=0 ;
-            try
-            {
-                luongnv = int.Parse(txtLuong.Text);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Kiểu dữ liệu của lương phải là kiểu số !");
-            }
-            if (txtMaNv.Text.Trim() == "")
-                MessageBox.Show("Mã nhân viên không được để trống !");
-            else if (txtTenNv.Text.Trim() == "")
-                MessageBox.Show("Tên nhân viên không được để trống !");
-            else if (luongnv == 0)
-                MessageBox.Show("Thêm thất bại !");
-            else
-                nvb.insertNV(txtMaNv.Text, txtTenNv.Text, dtpNgaysinh.Value.ToString("dd/MM/yyyy"), txtDiachi.Text, luongnv, cbxPhongban.SelectedValue.ToString(), cbxDuan.SelectedValue.ToString());
-            Quanlynhanvien_Load(sender, e);
 
-        }
         private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
